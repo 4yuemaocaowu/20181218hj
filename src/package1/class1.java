9739a83001acb3d1321566e3a2c3ad811436923d
4yuemaocaowu <hjhsc307308@163.com>
2018-12-18 20:33:44 +0800

    Initial commit

---
+++ src/package1/class1.java
@@ -0,0 +1,11 @@
+package package1;
+
+/**
+ * @author HJ
+ * @date 2018/12/18-20:28
+ */
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("你好中国");
+    }
+}
